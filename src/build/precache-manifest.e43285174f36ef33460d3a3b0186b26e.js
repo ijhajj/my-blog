296804dcea0f585e210a9215772e17cc52396091
ijhajj <i.jhajj@gmail.com>
2020-04-27 12:46:39 +0530

    First Commit

---
+++ src/build/precache-manifest.e43285174f36ef33460d3a3b0186b26e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c3e24a21ae365f10c434612b729d2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e71fc67a0f567ecaca9",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "dc545c27ae1a54f1a242",
+    "url": "/static/js/2.5c4680b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5c4680b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e71fc67a0f567ecaca9",
+    "url": "/static/js/main.2d384853.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
